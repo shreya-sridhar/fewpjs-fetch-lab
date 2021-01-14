b1e84b94defc230904f25b90bbbc249dbb166ff4
shreya-sridhar <shreyasridhar1109@gmail.com>
2021-01-13 21:56:12 -0800

    done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
+  const allBooks = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 
+  return allBooks
 }
 
 function renderBooks(books) {
